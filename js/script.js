36dc136e204a2a75fe6f1708e28fc12a8e6f3f4b
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-12-03 22:53:35 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,13 +29,31 @@ function formatCountdown(unixTimestamp) {
   // Create a Date object using the Unix timestamp (multiply by 1000 to convert to milliseconds)
   const targetDate = new Date(unixTimestamp * 1000);
 
-  // Get the current date and time
-  const currentDate = new Date();
-  console.log(1, currentDate);
-  console.log(2, currentDate);
+  // Get current date/time
+  const now = new Date();
+
+  // Options for formatting the date/time
+  const options = {
+    timeZone: "America/New_York", // Set the time zone to Eastern Time
+    hour12: false, // Set to false if you want 24-hour time format
+    weekday: "long", // Display the full name of the day
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+
+  // Format the date/time in Eastern Time
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  const etTimeStr = formatter.format(now);
+
+  // Create a new Date object from the formatted string
+  const etTime = new Date(etTimeStr);
 
   // Calculate the time difference in milliseconds
-  const timeDifference = targetDate - currentDate;
+  const timeDifference = targetDate - etTime;
 
   // Calculate days, hours, and minutes
   const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
